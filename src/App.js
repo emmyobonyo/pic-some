8154fc1142ee539919b93f91cd71dfa22d7906ee
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-04-25 14:14:49 +0300

    Add Context Provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 import './App.css'
+import { sRoutes, Route } from 'react-router-dom'
 
 function App() {    
-    return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-        </div>
-    )
+  return (
+      <div>
+        <Header />
+        <h1>Home Page</h1>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </div>
+  )
 }
 
 export default App
